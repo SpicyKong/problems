a111a17c1070f8e14747972aa04b885332c96e19
SeoungWon Shin <ssw30951@gmail.com>
2019-07-31 23:19:41 +0900

    Update Q_10800.py
    소스 코드 추가

--- BOJ/Q_10800.py
+++ BOJ/Q_10800.py
@@ -21,3 +21,23 @@ for a in range(num):
     for c in list_color_find:
         asum += sum([d for d in list_input[c] if d < list_input[list_seq[a][0]][list_seq[a][1]]])
     print(asum)
+
+#___________________________________________________________________________________________________________
+# 밑은 다시 작성한 버전, 시간초과..
+
+num = int(input())
+#list_a = [input().split() for _ in range(num)]
+list_a = []
+list_b = [0 for _ in range(num)]
+for a in range(num):
+    input_ob = input().split()
+    input_ob[0] = int(input_ob[0])
+    input_ob[1] = int(input_ob[1])
+    list_a.append(input_ob)
+    for b in range(a+1):
+        if not list_a[b][0] == input_ob[0]:
+            if input_ob[1] > list_a[b][1]:
+                list_b[a] += list_a[b][1]
+            elif input_ob[1] < list_a[b][1]:
+                list_b[b] += input_ob[1]
+[print(c) for c in list_b]
