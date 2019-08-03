fad3c402e43b24c7375d5a5c92bfb0eb9e0ef444
SeoungWon Shin <ssw30951@gmail.com>
2019-08-04 00:41:04 +0900

    Update Q_2869.py

--- BOJ/Q_2869.py
+++ BOJ/Q_2869.py
@@ -1,3 +1,5 @@
+# https://www.acmicpc.net/problem/2869 문제 제목 : 달팽이는 올라가고 싶다 , 언어 : Python, 날짜 : 20190803, 결과 : 성공
+
 data = [int(b) for b in input().split()]
 day = data[0]- data[1]
 days = data[2]/day
