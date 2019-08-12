58709cca6c57f180aa0e15c902b8178c1bfa8353
SeoungWon Shin <ssw30951@gmail.com>
2019-08-12 22:54:09 +0900

    Update Q_7576.py
    수정버전, 실행시간은 단축 됬으나 프로그램 구상에 오류가 있던거 같다.

--- BOJ/Q_7576.py
+++ BOJ/Q_7576.py
@@ -1,4 +1,6 @@
-# https://www.acmicpc.net/problem/7576 문제 제목 : 토마토 , 언어 : Python, 날짜 : 2019-08-12, 결과 : 실패(시간초과)
+# https://www.acmicpc.net/problem/7576 문제 제목 : 토마토 , 언어 : Python, 날짜 : 2019-08-12, 결과 : 실패(틀렸습니다!)
+#   기존의 방법에서 불필요한 조건문을 없애서 확실히 실행시간 단축에 도움이 되긴 했는데
+#   계속 하다보니 10% 지점에서 틀린다. 다시 수정해봐야 겠다.
 
 import sys
 
@@ -14,35 +16,74 @@ while(tomato):
         for c,d in enumerate(b):
             #print("asdf")
             #print(d)
-            if d=='1':
-                if not a==0 and not list_tomato[a-1][c] =='-1' and not list_tomato[a-1][c] =='1':
-                    list_tomato[a-1][c]='2'
-                    if thereistomato==0:
-                        thereistomato+=1
-                if not a == y-1 and not list_tomato[a+1][c] =='-1' and not list_tomato[a+1][c] =='1':
-                    list_tomato[a+1][c]='2'
-                    if thereistomato==0:
-                        thereistomato+=1
-                if not c==0 and not list_tomato[a][c-1] =='-1' and not list_tomato[a][c-1] =='1':
-                    list_tomato[a][c-1]='2'
-                    if thereistomato==0:
-                        thereistomato+=1
-                if not c == x-1 and not list_tomato[a][c+1] =='-1' and not list_tomato[a][c+1] =='1':
-                    list_tomato[a][c+1]='2'
-                    if thereistomato==0:
-                        thereistomato+=1
-    for a,b in enumerate(list_tomato):
-        for c,d in enumerate(b):
-            if d=='2':
-                list_tomato[a][c]='1'
+            if count%2==0 and d=='1':
+                try:
+                    if list_tomato[a-1][c] =='0':
+                        list_tomato[a-1][c]='2'
+                        if thereistomato==0:
+                            thereistomato+=1
+                except:
+                    pass
+                try:
+                    if list_tomato[a+1][c] =='0':
+                        list_tomato[a+1][c]='2'
+                        if thereistomato==0:
+                            thereistomato+=1
+                except:
+                    pass
+                try:
+                    if list_tomato[a][c-1] =='0':
+                        list_tomato[a][c-1]='2'
+                        if thereistomato==0:
+                            thereistomato+=1
+                except:
+                    pass
+                try:
+                    if list_tomato[a][c+1] =='0':
+                        list_tomato[a][c+1]='2'
+                        if thereistomato==0:
+                            thereistomato+=1
+                except:
+                    pass
+
+            elif count%2==1 and d=='2':
+                try:
+                    if list_tomato[a-1][c] =='0':
+                        list_tomato[a-1][c]='1'
+                        if thereistomato==0:
+                            thereistomato+=1
+                except:
+                    pass
+                try:
+                    if list_tomato[a+1][c] =='0':
+                        list_tomato[a+1][c]='1'
+                        if thereistomato==0:
+                            thereistomato+=1
+                except:
+                    pass
+                try:
+                    if list_tomato[a][c-1] =='0':
+                        list_tomato[a][c-1]='1'
+                        if thereistomato==0:
+                            thereistomato+=1
+                except:
+                    pass
+                try:
+                    if list_tomato[a][c+1] =='0':
+                        list_tomato[a][c+1]='1'
+                        if thereistomato==0:
+                            thereistomato+=1
+                except:
+                    pass
     if thereistomato==0:
         break
     count+=1
 asdf = 0
+#[print(a) for a in list_tomato]
 for a,b in enumerate(list_tomato):
-    for c,d in enumerate(b):
-        if d=='0':
-            print('-1')
-            asdf =1
+    if '0' in b:
+        print('-1')
+        asdf =1
+        break
 if asdf==0:
     print(count)
