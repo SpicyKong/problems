cddf84281cac296950c51dec76ffc1356487f8a7
SeoungWon Shin <ssw30951@gmail.com>
2019-08-16 23:14:48 +0900

    Create Q_10172.py

---
+++ BOJ/Q_10172.py
@@ -0,0 +1,7 @@
+# https://www.acmicpc.net/problem/10172 문제 제목 : 개 , 언어 : Python, 날짜 : 2019-08-16, 결과 : 성공
+
+print('|\_/|')
+print('|q p|   /}')
+print('( 0 )"""\\')
+print('|"^"`    |')
+print('||_/=\\\\__|')
