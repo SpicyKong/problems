0b766bf81e847909a2e750129f4b324305f9f358
SeoungWon Shin <ssw30951@gmail.com>
2019-08-24 23:09:57 +0900

    Update Q_2875.py
    전 버전은  PC방에서 메모장 코딩한거라 다시 집와서 풀어봤음.

--- BOJ/Q_2875.py
+++ BOJ/Q_2875.py
@@ -1,25 +1,29 @@
-# 푸는중..
+# https://www.acmicpc.net/problem/2875 문제 제목 : 대회 or 인턴 , 언어 : Python, 날짜 : 2019-08-24, 결과 : 성공
 
 import sys
 
 N, M, K = map(int, sys.stdin.readline().split())
-
-N_1 = N/2
-N_2 , M_2 = 0,0
-team_num = 0
-if N_1 > M:
-    team_num = M
-    N_2 = (N_1 - M) * 2
-elif N_1 < M:
-    team_num = N_1
-    M_2 = M - N_1
+if N > 0 and M > 0:
+    N_1 = N//2
+    N_2, M_2 = 0, 0
+    team_num = 0
+    if N_1 > M:
+        team_num += M
+        N_2 = N - M*2
+    elif N_1 < M:
+        team_num += N_1
+        M_2 = M - N_1
+    else:
+        team_num += M
+    need = N_2+M_2
+    if need >= K:
+        K=0
+    else:
+        K = K-need
+        a,b = divmod(K,3)
+        if b >=1:
+            a+=1
+        team_num-=a
+    print(team_num)
 else:
-    team_num = M
-if K - N_2+M_2 < 0:
-    K = 0
-else:
-    K-= N_2+M_2
-a,b = divmod(K, 3)
-if b>=1:
-    a+=1
-print(team_num)
+    print(0)
