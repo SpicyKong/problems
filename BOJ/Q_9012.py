7a1ba0a3ce1a38860e2083d4f82d68d7c77b9964
SeoungWon Shin <ssw30951@gmail.com>
2019-08-27 21:35:53 +0900

    Create Q_9012.py

---
+++ BOJ/Q_9012.py
@@ -0,0 +1,22 @@
+# https://www.acmicpc.net/problem/9012 문제 제목 : 괄호 , 언어 : Python, 날짜 : 2019-08-27, 결과 : 성공
+
+import sys
+for _ in range(int(sys.stdin.readline())):
+    re = True
+    list_a = []
+    for text in sys.stdin.readline():
+        if text == '(':
+            list_a.append('(')
+        elif len(list_a) > 0 and text == ')':
+            list_a.pop()
+        elif len(list_a) == 0 and text == ')':
+            re = False
+            break
+    #print(list_a)
+    if len(list_a) > 0:
+        #print("len")
+        re = False
+    if re == True:
+        print('YES')
+    else:
+        print('NO')
