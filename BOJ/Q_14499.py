9df90ca6f93e70feb74d5b01ff8b92371c27c7b8
SeoungWon Shin <ssw30951@gmail.com>
2019-09-07 10:50:45 +0900

    Update Q_14499.py

--- BOJ/Q_14499.py
+++ BOJ/Q_14499.py
@@ -1,6 +1,7 @@
-# https://www.acmicpc.net/problem/14499 문제 제목 : 주사위 굴리기 , 언어 : Python, 날짜 : 2019-09-06, 결과 : 실패
+# https://www.acmicpc.net/problem/14499 문제 제목 : 주사위 굴리기 , 언어 : Python, 날짜 : 2019-09-06 > 2019-09-07 , 결과 : 성공
 
-# https://www.acmicpc.net/problem/14499 문제 제목 : 주사위 굴리기 , 언어 : Python, 날짜 : 2019-09-06, 결과 : 실패
+# 이 문제는 문제를 진짜 잘 읽어 봐야한다.. x랑 y.. 낚여씀
+# 문제 읽는 연습 하기!
 
 import sys
 
@@ -14,10 +15,10 @@ def func_act(num):
     save_num = 0
     if num == 1:# [2, 4, 5, 3, 6, 1]
         save_num = dice[1]
-        dice[1] = dice[5]
-        dice[5] = dice[3]
-        dice[3] = dice[4]
-        dice[4] = save_num
+        dice[1] = dice[4]
+        dice[4] = dice[3]
+        dice[3] = dice[5]
+        dice[5] = save_num
     elif num == 2: #[2, 3, 5, 4, 1, 6]
         save_num = dice[1]
         dice[1] = dice[5]
@@ -40,33 +41,32 @@ def func_act(num):
 for a in range(N):
     list_a = list(map(int, sys.stdin.readline().split()))
     list_map[a]=list_a[:]
+
 for act in list(map(int, sys.stdin.readline().split())):
-    #[print(a) for a in list_map]
-    #print(dice)
-    #print("______")
+    
     q_act = 0
-    func_act(act)
     if act==1:
-        if x < M-1:
-            x+=1
+        if y < M-1:
+            y+=1
             q_act = 1
     elif act==2:
-        if x > 0:
-            x-=1
-            q_act = 1
-    elif act==3:
         if y > 0:
             y-=1
             q_act = 1
+    elif act==3:
+        if x > 0:
+            x-=1
+            q_act = 1
     elif act==4:
-        if y < N-1:
-            y+=1
+        if x < N-1:
+            x+=1
             q_act = 1
     if q_act ==1:
-        if list_map[y][x] == 0:
-            list_map[y][x] = dice[3]
+        
+        func_act(act)
+        if list_map[x][y] == 0:
+            list_map[x][y] = dice[3]
         else:
-            dice[3] = list_map[y][x]
-            list_map[y][x] = 0
+            dice[3] = list_map[x][y]
+            list_map[x][y] = 0
         print(dice[1])
-        #print("act",act,x,y,":",dice[1])
