d17a2f96fde25993afa7aa110d7c96ea90b6414b
SeoungWon Shin <ssw30951@gmail.com>
2019-09-08 19:18:29 +0900

    Update Q_1244.py

--- BOJ/Q_1244.py
+++ BOJ/Q_1244.py
@@ -1,4 +1,5 @@
 # https://www.acmicpc.net/problem/1244 문제 제목 : 스위치 켜고 끄기 , 언어 : Python, 날짜 : 2019-08-30, 결과 : 실패
+# https://www.acmicpc.net/problem/1244 문제 제목 : 스위치 켜고 끄기 , 언어 : Python, 날짜 : 2019-09-08, 결과 : 성공
 
 import sys
 
@@ -22,22 +23,20 @@ for _ in range(N_a):
         else:
             list_switch[n_s-1] = '1'
         while test:
-            try:
+            if 0<= n_s-1+count_a < N and 0<= n_s-1-count_a < N:
                 if list_switch[n_s-1+count_a] == list_switch[n_s-1-count_a]:
                     if list_switch[n_s-1+count_a] == '1':
                         list_switch[n_s-1+count_a]='0'
-                    else:
-                        list_switch[n_s-1+count_a]='1'
-                    if list_switch[n_s-1-count_a] == '1':
                         list_switch[n_s-1-count_a]='0'
                     else:
+                        list_switch[n_s-1+count_a]='1'
                         list_switch[n_s-1-count_a]='1'
                     count_a+=1
                 else:
                     test=False
-            except:
+            else:
                 test=False
-#print(' '.join(list_switch))
+    #print(list_switch)
 for b in range(1,N+1):
     print(list_switch[b-1],end=' ')
     if b%20==0:
