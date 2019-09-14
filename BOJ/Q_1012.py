e81ecb5592ef6a6ba25ab8252488122aedee7dcc
SeoungWon Shin <ssw30951@gmail.com>
2019-09-14 14:01:39 +0900

    Update Q_1012.py
    2시간 가량을 삽질하다가 도저히 모르겠어서 질문글 올리다가 해결됬어요ㅋㅋ

--- BOJ/Q_1012.py
+++ BOJ/Q_1012.py
@@ -1,48 +1,35 @@
 # https://www.acmicpc.net/problem/1012 문제 제목 : 유기농 배추 , 언어 : Python, 날짜 : 2019-08-25, 결과 : 실패
-
+# https://www.acmicpc.net/problem/1012 문제 제목 : 유기농 배추 , 언어 : Python, 날짜 : 2019-09-14, 결과 : 성공
 import sys
 
-N = int(sys.stdin.readline())
-
-
-
-for _ in range(N):
-    M, N, K = map(int, sys.stdin.readline().split())
+def contour():
+    global x, y, list_field, list_visit
+    queue_ismi = [[x, y]]
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    while queue_ismi:
+        x, y = queue_ismi.pop(0)
+        list_field[y][x] = 0
+        for i in range(4):
+            ax = x + dx[i]
+            ay = y + dy[i]
+            if 0 <= ax < n and 0 <= ay < m:
+                if list_field[ay][ax] and not list_visit[ay][ax]:
+                    list_visit[ay][ax] = 1
+                    queue_ismi.append([ax,ay])
+t = int(sys.stdin.readline())
+for _ in range(t):
+    n, m, k = map(int, sys.stdin.readline()[:-1].split())
+    list_field = [[0]*n for _ in range(m)]
+    list_visit = [[0]*n for _ in range(m)]
     list_bechu = []
+    for i in range(k):
+        a, b = map(int, sys.stdin.readline().split())
+        list_bechu.append([a, b])
+        list_field[b][a] = 1
     count = 0
-    for a in range(K):
-        bechu = list(map(int, sys.stdin.readline().split()))
-        if [bechu[0] -1, bechu[1]] in list_bechu or [bechu[0] +1, bechu[1]] in list_bechu or [bechu[0], bechu[1] -1] in list_bechu or [bechu[0], bechu[1] +1] in list_bechu:
-            list_bechu.append(bechu)
-        else:
-          list_bechu.append(bechu)
-          count+=1
-    print(list_bechu)
-    print("N :",count)
-    """
-    
-    import sys
-
-N = int(sys.stdin.readline())
-
-
-
-for _ in range(N):
-    M, N, K = map(int, sys.stdin.readline().split())
-    list_bechu = []
-    count = 1
-    for _ in range(K):
-        list_bechu.append(list(map(int, sys.stdin.readline().split())))
-    for bechu in list_bechu:
-        if [bechu[0] -1, bechu[1]] in list_bechu or [bechu[0] +1, bechu[1]] in list_bechu or [bechu[0], bechu[1] -1] in list_bechu or [bechu[0], bechu[1] +1] in list_bechu:
-            pass
-        else:
+    for x,y in list_bechu:
+        if list_field[y][x]==1:
+            contour()
             count+=1
-    #print(list_bechu)
-    print("N :",count)1
-2 2 3
-1 0
-0 1
-1 1
-    푸는 중인데, 자소서쓰느냐고 일단 보류
-    """
+    print(count)
