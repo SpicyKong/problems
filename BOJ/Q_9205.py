9b43818d9484888615c561e522a22da291fac150
SeoungWon Shin <ssw30951@gmail.com>
2019-09-25 17:39:27 +0900

    Update Q_9205.py

--- BOJ/Q_9205.py
+++ BOJ/Q_9205.py
@@ -1,29 +1,31 @@
 # https://www.acmicpc.net/problem/9205 문제 제목 : 맥주 마시면서 걸어가기 , 언어 : Python, 날짜 : 2019-09-24, 결과 : 실패
+# https://www.acmicpc.net/problem/9205 문제 제목 : 맥주 마시면서 걸어가기 , 언어 : Python, 날짜 : 2019-09-25, 결과 : 성공
 
-# 푸는중 입니다..
 import sys
 from collections import deque
 t = int(sys.stdin.readline())
-#state_home = []
 state_store = []
 state_fastiver = []
-n = int(sys.stdin.readline())
 for _ in range(t):
+    b_e = 0
+    n = int(sys.stdin.readline())
     state_home = list(map(int, sys.stdin.readline().split()))
     state_store = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
     state_fastiver = list(map(int, sys.stdin.readline().split()))
     if abs(state_home[0] - state_fastiver[0])+abs(state_home[1] - state_fastiver[1]) <= 1000:
         print("happy")
     else:
-        list_queue = deque()
-        for i,j in enumerate(state_store):
-            if abs(state_home[0] - j[0])+abs(state_home[1] - j[1]) <= 1000:
-                list_queue.append(i)
-                list_visit.append(1)
-            else:
-                list_visit.append(0)
-        #list_queue = deque([i for i,j in enumerate(state_store) if abs(state_home[0] - j[0])+abs(state_home[1] - j[1]) <= 1000 ])
-        #list_visit = 
+        list_queue = deque([i for i in range(n) if abs(state_home[0] - state_store[i][0])+abs(state_home[1] - state_store[i][1]) <= 1000])
+        list_visit = [0 for _ in range(n)]
         while list_queue:
             x, y = state_store[list_queue.popleft()]
-
+            if abs(x - state_fastiver[0])+abs(y - state_fastiver[1]) <= 1000:
+                print('happy')
+                b_e = 1
+                break
+            for i,j in enumerate(state_store):
+                if abs(x - j[0])+abs(y - j[1]) <= 1000 and not list_visit[i]:
+                    list_queue.append(i)
+                    list_visit[i] = 1
+        if b_e == 0:
+            print('sad')
