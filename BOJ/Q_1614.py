f957ddd8a6fa9549d341361deb315152d8626b18
SeoungWon Shin <ssw30951@gmail.com>
2019-09-27 22:44:07 +0900

    Update Q_1614.py

--- BOJ/Q_1614.py
+++ BOJ/Q_1614.py
@@ -1,28 +1,15 @@
 # https://www.acmicpc.net/problem/1614 문제 제목 : 영식이의 손가락 , 언어 : Python, 날짜 : 2019-09-26, 결과 : 실패
+# https://www.acmicpc.net/problem/1614 문제 제목 : 영식이의 손가락 , 언어 : Python, 날짜 : 2019-09-27, 결과 : 성공
 
 finger_num = int(input())
-max_num = int(input())
-result = 0
-if finger_num%4==1:
-    if finger_num == 1: # 1일때
-        result = (max_num)*8
-    else: # 5일때
-        result = (max_num)*8 + 4
+count_max = int(input())
+if finger_num == 1:
+    result = 8*count_max
+elif finger_num == 5:
+    result = 4+8*count_max
 else:
-    result = max_num*4 # 짝이면 + 홀이면 -
-    if (max_num)%2 == 0:
-        result+=finger_num-1
+    if count_max%2==0:
+        result = 4*count_max + finger_num - 1
     else:
-        result+=3
-        if finger_num==4:
-            finger_num-=2
+        result = 4*count_max + 5-finger_num
 print(result)
-"""
-1234
-5432
-1234
-5432
-1234
-5432
-
-"""
