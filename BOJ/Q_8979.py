47f8df608d49edef4989c2c934bf5d27098347bc
SeoungWon Shin <ssw30951@gmail.com>
2019-12-07 22:40:06 +0900

    Update Q_8979.py

--- BOJ/Q_8979.py
+++ BOJ/Q_8979.py
@@ -1,6 +1,9 @@
 # https://www.acmicpc.net/problem/8979 문제 제목 : 올림픽 , 언어 : Python, 날짜 : 2019-11-24, 결과 : 실패
-# 이 문제는 간단해 보여서 시도했는데 내일은 다른방법으로 풀어봐야겠다..
+# https://www.acmicpc.net/problem/8979 문제 제목 : 올림픽 , 언어 : Python, 날짜 : 2019-12-07, 결과 : 성공
+# 정답코드는 아래에있다.
 
+# ============================= code ver.1 (틀린코드) ===========================================
+# 이 문제는 간단해 보여서 시도했는데 내일은 다른방법으로 풀어봐야겠다..
 import sys
 
 def quick_sort(arr, len_arr):
@@ -49,3 +52,30 @@ for country in list_country:
         print(count_rank)
         break
     save_num = country[1]
+    
+# ============================= code ver.2 (정답코드) ===========================================
+# 사실 이 문제는 게시판에서 정렬이 필요없다는 힌트를 듣고서야 정답코드가 떠올랐다..
+
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+list_medal = [[] for  _ in range(N+1)]
+for _ in range(N):
+    nation, gold, silver, bronze = map(int, sys.stdin.readline().split())
+    list_medal[nation] = [gold, silver, bronze]
+goal_g, goal_s, goal_b = list_medal[K]
+
+#print(list_medal[K])
+count = 1
+for i in range(1,N+1):
+    if list_medal[i][0] > goal_g:
+        count+=1
+    elif list_medal[i][0] == goal_g:
+        if list_medal[i][1] > goal_s:
+            count+=1
+        elif list_medal[i][1] == goal_s:
+            if list_medal[i][2] > goal_b:
+                count+=1
+print(count)
+
+
