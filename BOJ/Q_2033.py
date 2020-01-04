6f5689da1eede7609c19761391d60e0d66b0dea2
SeoungWon Shin <ssw30951@gmail.com>
2020-01-04 21:56:28 +0900

    Update Q_2033.py

--- BOJ/Q_2033.py
+++ BOJ/Q_2033.py
@@ -1,48 +1,49 @@
 # https://www.acmicpc.net/problem/2033 문제 제목 : 반올림 , 언어 : Python, 날짜 : 2020-01-03, 결과 : 실패
 # 오늘은 하루종일 밖이라 코딩을 못한다..
+# 왜 스트링으로 푼건 틀리는지 모르겟다. 반례도 못찾겠다...
 import sys
-N = list(sys.stdin.readline()[:-1])
-#test_buffer = 2
-#print(len(N)-2)
-def reload(len_a, list_a):
-    for i in range(1,len_a+1):
-        if 0 == len_a - i and int(list_a[len_a - i]) >= 10:
-            list_a[len_a - i] = str(int(list_a[len_a - i]) - 10)
-            list_a = ['1'] + list_a
-            
-            #list_a[len_a - i -1] = str(int(list_a[len_a - i -1]) + 1)
-        elif int(list_a[len_a - i]) >= 10:
-            list_a[len_a - i] = str(int(list_a[len_a - i]) - 10)
-            list_a[len_a - i -1] = str(int(list_a[len_a - i -1]) + 1)
-    return list_a
+from collections import deque
+N = deque(sys.stdin.readline()[:-1])#list(map(int, list(sys.stdin.readline())))
+len_N = len(N)
+lesser = 10
+point = 2
+def re_new(len_list_a, list_a):
+    list_a = deque(list_a)
+    for i in range(1, len_list_a+1):
+        if i == len_list_a:
+            if int(list_a[len_list_a - i]) >= 10:
+                list_a[len_list_a - i] = str(int(list_a[len_list_a - i]) - 10)
+                list_a.appendleft('1')
+                len_list_a +=1
+            return len_list_a , list_a
+        #print("test :",len_list_a - i)
+        if int(list_a[len_list_a - i]) >= 10:
+            list_a[len_list_a - i - 1] = str(int(list_a[len_list_a - i - 1]) + 1)
+            list_a[len_list_a - i] = str(int(list_a[len_list_a - i]) - 10)
+        #2 -1 -1
 
-#print(list_a[len_a - i])
-#if int(list_a[len_a - i]) >= 10:
-#    
-#    pass
+
+while int("".join(N)) > lesser:
+    if int(N[len_N - point + 1]) >= 5:
+        N[len_N - point + 1] = '0'
+        N[len_N - point] = str(int(N[len_N - point]) + 1)
+    elif int(N[len_N - point + 1]) < 5:
+        N[len_N - point + 1] = '0'
+    point+=1
+    lesser*=10
+    len_N, N = re_new(len_N, N)
+print("".join(N))
 
 
-#len_a = len(N)
-#for i in range(len_a):
-#    #list_a
-#    print(N[len_a - i - 1]) 
-#    
+# https://www.acmicpc.net/problem/2033 문제 제목 : 반올림 , 언어 : Python, 날짜 : 2020-01-04, 결과 : 성공
+import sys
 
-    
-    
-    
-    
-        
-
-    
-length = len(N)
-for i in range(length-1):
-    #print(N[length-1 - i])
-    if int(N[length-1 - i]) >= 5:
-        N[length-1 - i] = '0'
-        N[length-1 - i - 1] = str(int(N[length-1 - i - 1]) + 1)
+N = int(sys.stdin.readline())
+standard_point = 10
+while N > standard_point:
+    if N%standard_point >= 5*standard_point//10:
+        N = N//standard_point * standard_point + standard_point
     else:
-        N[length-1 - i] = '0'
-    N = reload(length, N)
-#print(N)   
-print(int("".join(N)))
+        N = N//standard_point * standard_point
+    standard_point*=10
+print(N)
